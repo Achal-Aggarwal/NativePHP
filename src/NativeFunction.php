0537f64d050a68d05d2dd9daaebb0a193f54c947
Achal-Aggarwal <theachalaggarwal@gmail.com>
2017-09-16 20:07:04 +0530

    Make shitty code less shitty

---
+++ src/NativeFunction.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace NativePHP;
+
+class NativeFunction
+{
+    private static $_object_map = array();
+
+    private $inNamespace;
+    private $functionToMock;
+    private $substituteFunction;
+
+    private $inClass = null;
+    private $inFunction = null;
+
+    private function __construct($functionToMock = null, $inNamespace = '\\')
+    {
+        $this->functionToMock = $functionToMock;
+        $this->inNamespace = $inNamespace;
+
+        $this->substituteFunction = function() {
+            return null;
+        };
+    }
+
+    public static function getStub($functionToMock, $inNamespace)
+    {
+        if (!array_key_exists($functionToMock, self::$_object_map))
+        {
+            self::$_object_map[$functionToMock] = array();
+        }
+
+        if (!array_key_exists($inNamespace, self::$_object_map[$functionToMock]))
+        {
+            $object = new NativeFunction($functionToMock, $inNamespace);
+            self::$_object_map[$functionToMock][$inNamespace] = $object;
+            $object->generate();
+        }
+        else
+        {
+            $object = self::$_object_map[$functionToMock][$inNamespace];
+        }
+
+        return $object;
+    }
+
+    public function workAs($substituteFunction)
+    {
+        $this->substituteFunction = $substituteFunction;
+    }
+
+    public function inOnly($inClass, $inFunction = null)
+    {
+        //@todo make them array
+        $this->inClass = $inClass;
+        $this->inFunction = $inFunction;
+    }
+
+    private function getCaller($callTrace) {
+        $caller = (count($callTrace) >= 2) ? $callTrace[1] : $callTrace[0];
+
+        return (object) array(
+            'callingFunction' => $caller['function'],
+            'callingClass' => isset($caller['class']) ? $caller['class'] : null
+        );
+    }
+
+    private function isClassSatisfied($caller)
+    {
+        $classSatisfied = $this->inClass == null;
+
+        if (!$classSatisfied
+            && $this->inClass != null
+            && $caller->callingClass != null
+            && "$this->inNamespace\\$this->inClass" == $caller->callingClass) {
+            $classSatisfied = true;
+        }
+
+        return $classSatisfied;
+    }
+
+    private function isFunctionSatisfied($caller)
+    {
+        $functionSatisfied = $this->inFunction == null;
+
+        if (!$functionSatisfied && $this->inFunction != null) {
+            if ($this->inClass != null && $this->inFunction == $caller->callingFunction) {
+                $functionSatisfied = true;
+            } else if ($this->inClass == null
+                && "$this->inNamespace\\$this->inFunction" == $caller->callingFunction) {
+                $functionSatisfied = true;
+            }
+        }
+
+        return $functionSatisfied;
+    }
+
+    public function getSubstitutedFunction($callTrace)
+    {
+        $caller = $this->getCaller($callTrace);
+
+        if ($this->isClassSatisfied($caller) && $this->isFunctionSatisfied($caller))
+        {
+            return $this->substituteFunction;
+        }
+
+        return "\\$this->functionToMock";
+    }
+
+    private function generate()
+    {
+        $ns = $this->inNamespace;
+        $mock = $this->functionToMock;
+
+        $str = '
+        namespace ' . $ns . '{
+            function ' . $mock . '(){
+                $object = \NativePHP\NativeFunction::getStub("' . $mock . '","' . $ns . '");
+                $callback = $object->getSubstitutedFunction(debug_backtrace());
+                return call_user_func_array($callback, func_get_args());
+            }
+        }';
+
+        eval($str);
+    }
+}
---
+++ tests/NativeFunctionTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace NativePHP {
+
+    use PHPUnit\Framework\TestCase;
+
+    class NativeFunctionTest extends TestCase
+    {
+        public $checkDateStub;
+
+        protected function setUp()
+        {
+            $this->checkDateStub = \NativePHP\NativeFunction::getStub(
+                'checkdate', 'SomeNamespace');
+
+            $this->checkDateStub->workAs(array(new Substitutes,'custom'));
+        }
+
+        public function testShouldRunStubbedMethodInsteadOfActualOne()
+        {
+            $this->assertEquals(
+                "Month:7, Day:30, Year:2014\n",
+                (new \SomeNamespace\SomeClass)->SomeClassMethod()
+            );
+
+            $this->assertEquals(
+                "Month:7, Day:30, Year:2014\n",
+                \SomeNamespace\SomeMethod()
+            );
+        }
+
+        public function testShouldRunStubbedMethodInLimitedMethodScopeOnly()
+        {
+            $this->checkDateStub->inOnly(null, 'SomeMethod');
+
+            $this->assertEquals(
+                true,
+                (new \SomeNamespace\SomeClass)->SomeClassMethod()
+            );
+
+            $this->assertEquals(
+                "Month:7, Day:30, Year:2014\n",
+                \SomeNamespace\SomeMethod()
+            );
+        }
+
+        public function testShouldRunStubbedMethodInLimitedClassScopeOnly()
+        {
+            $this->checkDateStub->inOnly('SomeClass', null);
+
+            $this->assertEquals(
+                "Month:7, Day:30, Year:2014\n",
+                (new \SomeNamespace\SomeClass)->SomeClassMethod()
+            );
+
+            $this->assertEquals(
+                "Month:7, Day:30, Year:2014\n",
+                (new \SomeNamespace\SomeClass)->SomeMethod()
+            );
+
+            $this->assertEquals(
+                true,
+                \SomeNamespace\SomeMethod()
+            );
+        }
+
+        public function testShouldRunStubbedMethodInLimitedClassMethodScopeOnly()
+        {
+            $this->checkDateStub->inOnly('SomeClass', 'SomeClassMethod');
+
+            $this->assertEquals(
+                "Month:7, Day:30, Year:2014\n",
+                (new \SomeNamespace\SomeClass)->SomeClassMethod()
+            );
+
+            $this->assertEquals(
+                true,
+                (new \SomeNamespace\SomeClass)->SomeMethod()
+            );
+
+            $this->assertEquals(
+                true,
+                \SomeNamespace\SomeMethod()
+            );
+        }
+    }
+
+    class Substitutes {
+
+        public function custom($m, $d, $y)
+        {
+            return "Month:$m, Day:$d, Year:$y\n";
+        }
+    }
+}
+
+namespace SomeNamespace{
+    class SomeClass{
+        public function SomeClassMethod(){
+            return checkdate(7,30,2014);
+        }
+
+        public function SomeMethod(){
+            return checkdate(7,30,2014);
+        }
+    }
+
+    function SomeMethod(){
+        return checkdate(7,30,2014);
+    }
+}
